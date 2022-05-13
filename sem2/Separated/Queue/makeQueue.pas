1aeb6e72f90a1e66d4d27657e94ce87f99adc6e0
mmkenq <65103953+mmkenq@users.noreply.github.com>
2022-05-13 14:07:20 +0400

    Add files via upload

---
+++ sem2/Separated/Queue/makeQueue.pas
@@ -0,0 +1,43 @@
+﻿Type Ptr = ^Queu;
+  Queu = record
+    Int: integer;
+    Next: Ptr;
+  end;
+
+var Left, Right, Top: Ptr;
+Value: integer;
+
+procedure MakeQueue;
+var Ok: boolean;
+begin
+  Ok:= True; writeln('Write num');
+  readln(Value); New(Top);
+  if Value = 999 then
+  begin
+    Ok:=False;
+    Top^.Next:=Nil;
+  end;
+  
+  Top^.Int:= Value;
+  Right:=Top; Left:=Top;
+  while Ok do
+  begin
+    writeln('write num');
+    readln(Value);
+    if Value = 999 then
+    begin
+      Ok:=False; Top^.Next:=Nil;
+    end
+    else 
+    begin
+      New(Top);
+      Right^.Next := Top;
+      Top^.Int := Value;
+      Right:= Top;
+    end;
+  end;
+end;
+
+Begin
+  MakeQueue;  
+end.
---
+++ sem2/Separated/Queue/viewQueue.pas
@@ -0,0 +1,59 @@
+﻿Type Ptr = ^Queu;
+  Queu = record
+    Int: integer;
+    Next: Ptr;
+  end;
+
+var Left, Right, Top: Ptr;
+Value: integer;
+
+procedure MakeQueue;
+var Ok: boolean;
+begin
+  Ok:= True; writeln('Write num');
+  readln(Value); New(Top);
+  if Value = 999 then
+  begin
+    Ok:=False;
+    Top^.Next:=Nil;
+  end;
+  
+  Top^.Int:= Value;
+  Right:=Top; Left:=Top;
+  while Ok do
+  begin
+    writeln('write num');
+    readln(Value);
+    if Value = 999 then
+    begin
+      Ok:=False; Top^.Next:=Nil;
+    end
+    else 
+    begin
+      New(Top);
+      Right^.Next := Top;
+      Top^.Int := Value;
+      Right:= Top;
+    end;
+  end;
+end;
+
+procedure ViewQueue;
+var i: Ptr;
+begin
+  i:= Left;
+  while i^.Next <> Nil do
+  begin
+    writeln(i^.Int);
+    i:= i^.Next;
+  end;
+  
+  writeln(i^.Int);
+end;
+
+Begin
+  MakeQueue;
+  writeln;
+  writeln('VIEW QUEUE:');
+  ViewQueue;
+end.
